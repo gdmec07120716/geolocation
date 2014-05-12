2d1a0ee15196f3e738cab0afb51f22d4d6fbfcd9
chalin <583753003@qq.com>
2014-05-12 11:36:53 +0800

    fsdfa

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\dell\geolocation\platforms\android\bin\AndroidManifest.xml : \
+C:\Users\dell\geolocation\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\Users\dell\geolocation\platforms\android\bin\classes.dex : \
+C:\Users\dell\geolocation\platforms\android\bin\classes\cn\edu\gdmec\t\geolocation\BuildConfig.class \
+C:\Users\dell\geolocation\platforms\android\bin\classes\cn\edu\gdmec\t\geolocation\MyGeolocation.class \
+C:\Users\dell\geolocation\platforms\android\bin\classes\cn\edu\gdmec\t\geolocation\R$attr.class \
+C:\Users\dell\geolocation\platforms\android\bin\classes\cn\edu\gdmec\t\geolocation\R$drawable.class \
+C:\Users\dell\geolocation\platforms\android\bin\classes\cn\edu\gdmec\t\geolocation\R$string.class \
+C:\Users\dell\geolocation\platforms\android\bin\classes\cn\edu\gdmec\t\geolocation\R$xml.class \
+C:\Users\dell\geolocation\platforms\android\bin\classes\cn\edu\gdmec\t\geolocation\R.class \
+C:\Users\dell\geolocation\platforms\android\bin\dexedLibs\classes-baa83a6f0bd045db222d912025681a53.jar \
